f02f92dbffb4444ecaecf88cedc51966f70369ec
Manny <jflore16@emich.edu>
2025-11-14 01:08:27 -0500

    Database Setup Code
    Added code to set up our database and some to get cart items based on user email

---
+++ backend/getCartItems.php
@@ -0,0 +1,28 @@
+<?php
+
+//Retrieves all cart items based on user email
+$servername = "localhost";
+$username = "admin";
+$password = "password";
+$dbname = "ClothingStore";
+
+$customeremail = "John@test.com";
+
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $stmt = $conn->prepare("SELECT product_id FROM CartItems WHERE user_email = '$customeremail'");
+  $stmt->execute();
+
+  // set the resulting array to associative
+  $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+  foreach(new RecursiveArrayIterator($stmt->fetchAll()) as $k=>$v) {
+    echo $k.current($v);
+    echo "<br>";
+  }
+} catch(PDOException $e) {
+  echo "Error: " . $e->getMessage();
+}
+$conn = null;
+?>
